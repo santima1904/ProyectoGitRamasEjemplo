5a5d71e475b6c48583a79a43fb2f01f176535ea6
santima1904 <75076868+santima1904@users.noreply.github.com>
2020-12-03 14:23:56 +0100

    Update PreyectoGitRamasEjemplo.java

--- src/PreyectoGitRamasEjemplo.java
+++ src/PreyectoGitRamasEjemplo.java
@@ -1,5 +1,9 @@
 
 public class PreyectoGitRamasEjemplo {
+	public static void imprimeSaludo() { 
+		System.out.println(“Hei”);	
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 	}
